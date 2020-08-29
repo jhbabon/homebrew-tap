e57e1f014db7c87a4ff5397a78bb655b27dc2cd7
Juan Hernández <juan.hernandez.babon@gmail.com>
2020-08-29 12:58:46 +0200

    Fix sha256 sum for scout file

--- scout.rb
+++ scout.rb
@@ -2,7 +2,7 @@ class Scout < Formula
   desc "Friendly fuzzy finder for your command line"
   homepage "https://github.com/jhbabon/scout"
   url "https://github.com/jhbabon/scout/archive/v2.2.0.tar.gz"
-  sha256 "52f7ac031e8b98bc6007765c0af781211f82da037f24eb06531a2ad508f6007"
+  sha256 "52f7ac031e8b98bc6007765c0af781211f82da037f24eb06531a2ad508f6007c"
 
   depends_on "rust" => :build
 
