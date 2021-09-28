e305c504d32368f1a158dce7463c665e8f1bd632
Juan Hernández <juan.hernandez.babon@gmail.com>
2021-09-28 22:54:09 +0200

    Fix scout sha256

--- scout.rb
+++ scout.rb
@@ -2,7 +2,7 @@ class Scout < Formula
   desc "Friendly fuzzy finder for your command line"
   homepage "https://github.com/jhbabon/scout"
   url "https://github.com/jhbabon/scout/archive/v2.5.1.tar.gz"
-  sha256 "cc986c1a3bf608ff7ee277d92565ee29e5d634c0c7ec4258f2131786d488a7f3"
+  sha256 "d1d1c0d92b0a252e014fb52837baed8541b0edb55f7ebf74e104d0b245f007a9"
 
   depends_on "rust" => :build
 
