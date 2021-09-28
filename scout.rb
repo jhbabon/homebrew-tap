8f67c2f9cb7e619198c94b6274c49e48e346394e
Juan Hernández <juan.hernandez.babon@gmail.com>
2021-09-28 22:24:10 +0200

    Update scout to v2.5.1

--- scout.rb
+++ scout.rb
@@ -1,8 +1,8 @@
 class Scout < Formula
   desc "Friendly fuzzy finder for your command line"
   homepage "https://github.com/jhbabon/scout"
-  url "https://github.com/jhbabon/scout/archive/v2.4.1.tar.gz"
-  sha256 "cda300c28b1686e35746aaa592b3f3fdf4f168e345499a00c554f21c1b371fa0"
+  url "https://github.com/jhbabon/scout/archive/v2.5.1.tar.gz"
+  sha256 "cc986c1a3bf608ff7ee277d92565ee29e5d634c0c7ec4258f2131786d488a7f3"
 
   depends_on "rust" => :build
 
